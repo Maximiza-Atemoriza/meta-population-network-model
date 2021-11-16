da32fc1bc4070c653a4c07e2a4c53aa517a2be78
Adrian Portales <adrianportales135@gmail.com>
2021-11-16 17:04:43 -0500

    Add  dash-bootstrap-components

--- dash_app/main.py
+++ dash_app/main.py
@@ -3,20 +3,26 @@ import dash_html_components as html
 import dash_core_components as dcc
 import plotly.graph_objects as go
 import plotly.express as px
+import pandas as pd
+import dash_bootstrap_components as dbc
+from dash.dependencies import Input, Output
 
 
-app = dash.Dash()
+app = dash.Dash(__name__)
+app = dash.Dash(external_stylesheets=[dbc.themes.CERULEAN])
 
-app.layout = html.Div(
-    id="parent",
+badge = dbc.Button(id='button',
     children=[
-        html.H1(
-            id="H1",
-            children="Testing",
-            style={"textAlign": "center", "marginTop": 40, "marginBottom": 40},
-        ),
+        
+        "Notifications",
+        dbc.Badge("4", color="light", text_color="primary", className="ms-1"),
     ],
+    color="primary",
 )
 
+# Then we incorporate the snippet into our layout.
+# This example keeps it simple and just wraps it in a Container
+app.layout = dbc.Container(badge, fluid=True)
+
 if __name__ == "__main__":
-    app.run_server()
+    app.run_server(debug=True)
