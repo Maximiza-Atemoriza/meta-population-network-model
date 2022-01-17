5019e5905afef7889167a7b826fadccfd5db5ee8
Vitico99 <nenevictor99@gmail.com>
2022-01-16 22:01:11 -0500

    Add test compartmental model

--- cmodel/repo/SIR.py
+++ cmodel/repo/SIR.py
@@ -2,16 +2,22 @@ from scipy.integrate import odeint
 class SIR:
 
 	sets = ['S', 'I', 'R']
-	params = ['N', 'beta', 'gamma']
+	params = ['beta', 'gamma', 'N']
+	equations = {
+		'S' : lambda S,I,R,beta,gamma,N: f' -({beta} * {S} * {I}) / ({N})',
+		'I' : lambda S,I,R,beta,gamma,N: f' ({beta} * {S} * {I}) / ({N}) - {gamma} * {I}',
+		'R' : lambda S,I,R,beta,gamma,N: f' {gamma} * {I}',
+	}
 
 	@staticmethod
-	def deriv(initial_conditions, t, N, beta, gamma):
-		S, I, R = initial_conditions
+	def deriv(y, t, params):
+		S, I, R = y
+		beta, gamma, N = params
 		dSdt = -(beta * S * I) / (N)
 		dIdt = (beta * S * I) / (N) - gamma * I
 		dRdt = gamma * I
 		return dSdt, dIdt, dRdt
 
 	@staticmethod
-	def solve(initial_conditions, t, params):
-		return odeint(SIR.deriv, initial_conditions, t, args=params)
+	def solve(y, t, params):
+		return odeint(SIR.deriv, y, t, args=(params,))
