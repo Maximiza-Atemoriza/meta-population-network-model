ce3276b27639ffc5b4b91db5953b09821d097478
Vitico99 <nenevictor99@gmail.com>
2022-01-19 14:32:58 -0500

    Rebase interface to accept network source files

--- dash_app/app.py
+++ dash_app/app.py
@@ -1,13 +1,9 @@
 import dash
 import dash_bootstrap_components as dbc
-from repository import ModelRepository
 # bootstrap theme
 # https://bootswatch.com/lux/
 external_stylesheets = [dbc.themes.BOOTSTRAP]
-ModelRepository.start('models')
-
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
 server = app.server
 app.config.suppress_callback_exceptions = True
