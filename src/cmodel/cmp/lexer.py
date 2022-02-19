b3f4c692a24078c7d6e194c5d920effc4a629665
Vitico99 <nenevictor99@gmail.com>
2022-02-18 20:29:07 -0500

    Add suport for global symbols in equation definition

--- src/cmodel/cmp/lexer.py
+++ src/cmodel/cmp/lexer.py
@@ -1,22 +1,24 @@
 import ply.lex as lex
 
+
 class Lexer:
     tokens = [
-        'SYMBOL', # A single letter
-        'NUMBER', # An integer or decimal
-        'MACRO', # LaTex macros
-        'PLUS', # +
-        'MINUS', # -
-        'FRAC', # Special case of macro that works as division
-        'STAR', # *
-        'EQUAL', # =
-        'OPAR', # (
-        'CPAR', # )
-        'OCUR', # {
-        'CCUR', # }
-        'UNDER', # _
-        'DIF', # Symbol and macro used in the differential (d or \partial)
-        'LBREAK', # \\
+        "SYMBOL",  # A single letter
+        "NUMBER",  # An integer or decimal
+        "MACRO",  # LaTex macros
+        "PLUS",  # +
+        "MINUS",  # -
+        "FRAC",  # Special case of macro that works as division
+        "STAR",  # *
+        "EQUAL",  # =
+        "OPAR",  # (
+        "CPAR",  # )
+        "OCUR",  # {
+        "CCUR",  # }
+        "UNDER",  # _
+        "DIF",  # Symbol and macro used in the differential (d or \partial)
+        "LBREAK",  # \\
+        "PERCENT",
     ]
 
     def __init__(self):
@@ -25,37 +27,39 @@ class Lexer:
     def build(self):
         self.lexer = lex.lex(module=self)
 
-    def tokenize(self,text):
+    def tokenize(self, text):
         self.lexer.input(text)
         for t in self.lexer:
             yield t
 
-    t_NUMBER = r'\d+(\.\d*)?'
-    def t_SYMBOL(self, t): 
-        r'[a-zA-Z]'
-        if t.value == 'd':
-            t.type = 'DIF'
+    t_NUMBER = r"\d+(\.\d*)?"
+
+    def t_SYMBOL(self, t):
+        r"[a-zA-Z]"
+        if t.value == "d":
+            t.type = "DIF"
         return t
 
     def t_MACRO(self, t):
-        r'\\[a-zA-Z]+'
+        r"\\[a-zA-Z]+"
         t.value = t.value[1:]
-        if t.value.lower() == 'frac':
-            t.type ='FRAC'
-        elif t.value.lower() == 'cdot':
-            t.value = '*'
-            t.type = 'STAR'
-        elif t.value == 'partial':
-            t.type = 'DIF'
+        if t.value.lower() == "frac":
+            t.type = "FRAC"
+        elif t.value.lower() == "cdot":
+            t.value = "*"
+            t.type = "STAR"
+        elif t.value == "partial":
+            t.type = "DIF"
         return t
 
-    t_PLUS = r'\+'
-    t_MINUS = r'-'
-    t_EQUAL = r'='
-    t_OPAR = r'\('
-    t_CPAR = r'\)'
-    t_OCUR =  r'{'
-    t_CCUR = r'}'
-    t_UNDER = r'_'
-    t_LBREAK = r'\\\\'
-    t_ignore = ' \t\r\n'
+    t_PLUS = r"\+"
+    t_MINUS = r"-"
+    t_EQUAL = r"="
+    t_OPAR = r"\("
+    t_CPAR = r"\)"
+    t_OCUR = r"{"
+    t_CCUR = r"}"
+    t_UNDER = r"_"
+    t_LBREAK = r"\\\\"
+    t_PERCENT = r"%"
+    t_ignore = " \t\r\n"
--- src/cmodel/cmp/parser.py
+++ src/cmodel/cmp/parser.py
@@ -16,140 +16,135 @@ class Parser:
 
     @staticmethod
     def p_system(p):
-        'system : equation_list'
+        "system : equation_list"
         p[0] = SystemNode(p[1])
-    
+
     @staticmethod
     def p_equation_list_single(p):
-        'equation_list : equation'
+        "equation_list : equation"
         p[0] = [p[1]]
-    
+
     @staticmethod
     def p_equation_list_multi(p):
-        'equation_list : equation LBREAK equation_list'
+        "equation_list : equation LBREAK equation_list"
         p[0] = [p[1]] + p[3]
 
     @staticmethod
     def p_equation(p):
-        'equation : differential EQUAL expression'
+        "equation : differential EQUAL expression"
         p[0] = EquationNode(p[1], p[3])
-    
+
     @staticmethod
     def p_differential(p):
-        'differential : FRAC OCUR DIF identifier CCUR OCUR DIF identifier CCUR'
+        "differential : FRAC OCUR DIF identifier CCUR OCUR DIF identifier CCUR"
         p[0] = DifferentialNode(p[4], p[8])
 
     @staticmethod
     def p_fraction(p):
-        'fraction : FRAC OCUR expression CCUR OCUR expression CCUR'
+        "fraction : FRAC OCUR expression CCUR OCUR expression CCUR"
         p[0] = FractionNode(p[3], p[6])
 
     @staticmethod
     def p_expression(p):
-        'expression : arith'
+        "expression : arith"
         p[0] = p[1]
 
     @staticmethod
     def p_neg_expression(p):
-        'expression : MINUS expression'
+        "expression : MINUS expression"
         p[0] = NegNode(p[2])
-    
+
     @staticmethod
     def p_arith_term(p):
-        'arith : term'
+        "arith : term"
         p[0] = p[1]
-    
+
     @staticmethod
     def p_arith_plus(p):
-        'arith : arith PLUS term'
+        "arith : arith PLUS term"
         p[0] = PlusNode(p[1], p[3])
 
     @staticmethod
     def p_arith_minus(p):
-        'arith : arith MINUS term'
+        "arith : arith MINUS term"
         p[0] = MinusNode(p[1], p[3])
 
     # @staticmethod
     # def p_term_fac(p):
     #     'term : factor'
     #     p[0] = p[1]
-    
+
     @staticmethod
     def p_term_star(p):
-        'term : term STAR factor'
-        p[0] = StarNode(p[1],p[3])
+        "term : term STAR factor"
+        p[0] = StarNode(p[1], p[3])
 
     @staticmethod
     def p_term_idlist(p):
-        'term : idlist'
+        "term : idlist"
         p[0] = p[1]
-    
+
     @staticmethod
     def p_idlist_multi(p):
-        'idlist : factor idlist'
+        "idlist : factor idlist"
         p[0] = StarNode(p[1], p[2])
-    
+
     @staticmethod
     def p_idlist_single(p):
-        'idlist : factor'
+        "idlist : factor"
         p[0] = p[1]
 
     @staticmethod
     def p_factor_fraction(p):
-        'factor : fraction'
+        "factor : fraction"
         p[0] = p[1]
-    
+
     @staticmethod
     def p_factor_num(p):
-        'factor : NUMBER'
+        "factor : NUMBER"
         p[0] = NumberNode(p[1])
 
     @staticmethod
     def p_factor_pexpression(p):
-        'factor : OPAR expression CPAR'
+        "factor : OPAR expression CPAR"
         p[0] = ParenthesisNode(p[2])
 
     @staticmethod
     def p_factor_symbol(p):
-        'factor : identifier'
+        "factor : identifier"
         p[0] = p[1]
 
     @staticmethod
+    def p_factor_symbol_global(p):
+        "factor : PERCENT identifier"
+        p[0] = GlobalNode(p[2])
+
+    @staticmethod
     def p_symbol_atom(p):
-        'identifier : atom'
+        "identifier : atom"
         p[0] = p[1]
 
     @staticmethod
     def p_atom_symbol(p):
-        'atom : SYMBOL'
+        "atom : SYMBOL"
         p[0] = SymbolNode(p[1])
 
     @staticmethod
     def p_atom_macro(p):
-        'atom : MACRO'
+        "atom : MACRO"
         p[0] = SymbolNode(p[1])
-
 
     @staticmethod
     def p_symbol_comp(p):
-        'identifier : atom UNDER atom'
+        "identifier : atom UNDER atom"
         p[0] = IdentifierNode(p[1], p[3])
 
     @staticmethod
     def p_symbol_comp1(p):
-        'identifier : atom UNDER NUMBER'
+        "identifier : atom UNDER NUMBER"
         p[0] = IdentifierNode(p[1], NumberNode(p[3]))
-    
+
     @staticmethod
     def p_symbol_comp2(p):
-        'identifier : atom UNDER OCUR identifier CCUR'
+        "identifier : atom UNDER OCUR identifier CCUR"
         p[0] = IdentifierNode(p[1], p[4])
-
- 
-
-    
-        
-    
-
-
-
