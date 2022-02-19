ae59057497c2d55daa3a5cba9b5d86fac172e32b
Vitico99 <nenevictor99@gmail.com>
2022-02-18 23:01:34 -0500

    Add SIS and SIRS support for global sets in Simple Trip

--- src/cmodel/repo/SIRS.py
+++ src/cmodel/repo/SIRS.py
@@ -1,25 +1,25 @@
 from scipy.integrate import odeint
-
-
 class SIRS:
 
-    sets = ["S", "I", "R"]
-    params = ["beta", "gamma", "xi"]
-    equations = {
-        "S": lambda S, I, R, beta, gamma, xi: f" -({beta} * {S} * {I}) + {xi}*{R}",
-        "I": lambda S, I, R, beta, gamma, xi: f" ({beta} * {S} * {I})  - {gamma} * {I}",
-        "R": lambda S, I, R, beta, gamma, xi: f" {gamma} * {I} - {xi}*{R}",
-    }
+	sets = ['S', 'I', 'R', 'N']
+	params = ['xi', 'beta', 'gamma']
+	equations = {
+		'S' : lambda S,I,R,N,_S,_I,_R,_N,xi,beta,gamma: f' -({beta} * {S} * {_I}) / ({_N}) + {xi} * {R}',
+		'I' : lambda S,I,R,N,_S,_I,_R,_N,xi,beta,gamma: f' ({beta} * {S} * {_I}) / ({_N}) - {gamma} * {I}',
+		'R' : lambda S,I,R,N,_S,_I,_R,_N,xi,beta,gamma: f' {gamma} * {I} - {xi} * {R}',
+		'N' : lambda S,I,R,N,_S,_I,_R,_N,xi,beta,gamma: f' 0',
+	}
 
-    @staticmethod
-    def deriv(y, t, params):
-        S, I, R = y
-        beta, gamma, xi = params
-        dSdt = -(beta * S * I) + xi*R 
-        dIdt = (beta * S * I) - gamma * I
-        dRdt = gamma * I - xi*R
-        return dSdt, dIdt, dRdt
+	@staticmethod
+	def deriv(y, t, params):
+		S, I, R, N = y
+		xi, beta, gamma = params
+		dSdt = -(beta * S * I) / (N) + xi * R
+		dIdt = (beta * S * I) / (N) - gamma * I
+		dRdt = gamma * I - xi * R
+		dNdt = 0
+		return dSdt, dIdt, dRdt, dNdt
 
-    @staticmethod
-    def solve(y, t, params):
-        return odeint(SIRS.deriv, y, t, args=(params,))
+	@staticmethod
+	def solve(y, t, params):
+		return odeint(SIRS.deriv, y, t, args=(params,))
--- src/cmodel/repo/SIS.py
+++ src/cmodel/repo/SIS.py
@@ -1,23 +1,23 @@
 from scipy.integrate import odeint
-
-
 class SIS:
 
-    sets = ["S", "I"]
-    params = ["beta", "alpha"]
-    equations = {
-        "S": lambda S, I, beta, alpha: f"-{beta} * {S} * {I} + {alpha} * {I}",
-        "I": lambda S, I, beta, alpha: f"{beta} * {S} * {I} - {alpha} * {I}",
-    }
+	sets = ['S', 'I', 'N']
+	params = ['beta', 'gamma']
+	equations = {
+		'S' : lambda S,I,N,_S,_I,_N,beta,gamma: f' -({beta} * {S} * {_I}) / ({_N}) + {gamma} * {I}',
+		'I' : lambda S,I,N,_S,_I,_N,beta,gamma: f' ({beta} * {S} * {_I}) / ({_N}) - {gamma} * {I}',
+		'N' : lambda S,I,N,_S,_I,_N,beta,gamma: f' 0',
+	}
 
-    @staticmethod
-    def deriv(y, t, params):
-        S, I = y
-        beta, alpha = params
-        dSdt = -(beta * S * I) + alpha * I
-        dIdt = beta * S * I - alpha * I
-        return dSdt, dIdt
+	@staticmethod
+	def deriv(y, t, params):
+		S, I, N = y
+		beta, gamma = params
+		dSdt = -(beta * S * I) / (N) + gamma * I
+		dIdt = (beta * S * I) / (N) - gamma * I
+		dNdt = 0
+		return dSdt, dIdt, dNdt
 
-    @staticmethod
-    def solve(y, t, params):
-        return odeint(SIS.deriv, y, t, args=(params,))
+	@staticmethod
+	def solve(y, t, params):
+		return odeint(SIS.deriv, y, t, args=(params,))
